29421d41d53f42d357cfeb3141a0f37502dd73e9
Amoghyels <78698000+Amoghyels@users.noreply.github.com>
2022-11-28 22:08:45 +0530

    Data Structures

---
+++ src/StringsJava/Performance.java
@@ -0,0 +1,14 @@
+package StringsJava;
+
+public class Performance {
+    public static void main(String[] args) {
+        String series = "";
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            series = series + ch;
+
+        }
+        System.out.println(series);
+
+    }
+}
