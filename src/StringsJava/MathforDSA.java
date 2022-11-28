3e1b9e3213f984b64bc63e95541e1f8fb8ccef0c
Amoghyels <78698000+Amoghyels@users.noreply.github.com>
2022-11-28 12:12:13 +0530

    Data Structures

---
+++ src/StringsJava/MathforDSA.java
@@ -0,0 +1,13 @@
+package StringsJava;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class MathforDSA {
+    public static void main(String[] args) {
+
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a+b);
+    }
+}
