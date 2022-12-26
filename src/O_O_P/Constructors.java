73b78d3bc53acbe0243138ad5311d538cb9496d4
Amoghyels <78698000+Amoghyels@users.noreply.github.com>
2022-12-26 21:59:44 +0530

    Data Structures

--- src/O_O_P/Constructors.java
+++ src/O_O_P/Constructors.java
@@ -4,19 +4,25 @@ public class Constructors {
     public static void main(String[] args) {
         students[] students = new students[5];
         String[] name = new String[5];
-        float[] marks =  new float[5];
+        int[] marks =  new int[5];
 
 
-        O_O_P.students kunal  = new students();
+        O_O_P.students kunal  = new students(15,"Kunal", 85);
+        O_O_P.students rahul = new students();
      /*   kunal.name = "Amogh";
         kunal.rno = 13;
         kunal.marks = 87;*/
 
-       kunal.greeting();
 
-        /* System.out.println(kunal.name);
+      /* kunal.changename("Bro");
+        kunal.greeting();*/
+
+        System.out.println(kunal.name);
         System.out.println(kunal.marks);
-        System.out.println(kunal.rno);*/
+        System.out.println(kunal.rno);
+
+        students random = new students(kunal);
+        System.out.println(random.name);
 
 
     }
@@ -28,15 +34,35 @@ class students {
     int marks;
 
     void greeting(){
+
         System.out.println("Hello my name is " + name);
+    }
+
+    void changename(String newName){
+        name = newName;
     }
     //we need a way to add the values of the above properties
     //object by object
 
+
+    //CONSTRUCTOR OVERLOADING
+
+    students(students other){
+        this.name = other.name;
+        this.rno = other.rno;
+        this.marks = other.marks;
+    }
+
     students(){
-        this.rno = 13;
-        this.name = "Kunal kushwaha";
-        this.marks = 88;
+
+
+    }
+     //Student arpit = new Student(17, "Arpit", 89.7f);
+     // Here, "this" will be replaced with arpit
+     students(int rno, String name, int marks){
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
 
     }
 
